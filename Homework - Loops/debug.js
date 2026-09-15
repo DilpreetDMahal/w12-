12af4c2a5562dd623b19b155e1bc4a75b771c135
DilpreetDMahal <dsmgooner@gmail.com>
2026-09-15 18:25:01 -0400

    Loops

---
+++ Homework - Loops/debug.js
@@ -0,0 +1,88 @@
+// ============================================================
+// 🐛  LOOPS — HOMEWORK  |  DEBUG TASKS
+// ============================================================
+// Fix the bug in each snippet.
+// Explain what was wrong as a comment. Then fix it.
+// ============================================================
+
+
+// ----------------------------------------------------------
+// 🟢 DEBUG 1 — Easy
+// ----------------------------------------------------------
+// This loop should log numbers 1 through 10.
+// It only logs 1 through 9. What's wrong?
+
+for (let i = 1; i < 10; i++) {
+  console.log(i);
+}
+
+// What's wrong ↓
+
+// for (let i = 1; i < 10; i++)
+// Your fix ↓
+
+// // for (let i = 1; i <= 10; i++)
+
+// ----------------------------------------------------------
+// 🟡 DEBUG 2 — Medium
+// ----------------------------------------------------------
+// This loop should calculate the sum of 1 through 5 (answer: 15).
+// It always logs 0. What's wrong?
+
+
+let total = 0;
+for (let i = 1; i <= 5; i++) {
+  
+  total += i;
+}
+console.log("Sum: " + total);
+
+// What's wrong ↓
+
+// for (let i = 1; i <= 5; i++) {
+//   let total = 0;
+//   total += i;
+// }
+
+// after every loop it re assigning total, total should assigned outside the loop
+
+// Your fix ↓
+
+// let total = 0;
+
+// for (let i = 1; i <= 5; i++) {
+//   total += i;
+// }
+// console.log("Sum: " + total);
+
+
+// ----------------------------------------------------------
+// 🔴 DEBUG 3 — Hard
+// ----------------------------------------------------------
+// This loop should log all ODD numbers from 1 to 10,
+// then log "Done!" at the end.
+// Instead it logs nothing and skips straight to "Done!".
+// There are TWO bugs. Find both.
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    continue;  
+  } else {
+    console.log(i);
+  }
+}
+console.log("Done!");
+
+// Bug 1 ↓
+// if (i % 2 === 0) {
+//     console.log(i);
+//   }
+//  there should be continue here with console.log()
+// Bug 2 ↓
+
+//  } else {
+//     continue;
+//   }
+
+// no continue, only console.log(i)
+// Your fix ↓
